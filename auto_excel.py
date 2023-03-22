3b4e699ea4b06384cb2bf97c74211eb1fa1a1fc7
PickledPenguin <79871674+PickledPenguin@users.noreply.github.com>
2023-03-22 14:06:23 -0400

    Merge was being funky, so I'm manually copying over the files

--- auto_excel.py
+++ auto_excel.py
@@ -6,12 +6,13 @@ import pandas as pd
 import time
 import json
 import setup
+import datetime
 
 CURRENT_DIR = os.getcwd()
 EXCEL_DIR = "Excel"
 EXCEL_FILENAMES = os.listdir(f"./{EXCEL_DIR}")
 WAIT_DELAY_IN_SECONDS = 0.5
-sheet_name = "Time Sheet"
+DATETIME_FORMAT_STR = "default"
 
 Mouse = pynput.mouse.Controller()
 kb = pynput.keyboard.Controller()
@@ -22,227 +23,94 @@ def wait():
     time.sleep(WAIT_DELAY_IN_SECONDS)
 
 
-class ExcelData:
-    """ Excel class to store all relevant information from a student employee's Excel file """
-    def __init__(self, faculty_name, course, student_name, badge_num, timesheet):
-        self.faculty_name = faculty_name
-        self.course = course
-        self.student_name = student_name
-        self.badge_num = badge_num
-        self.timesheet = timesheet
-
-
 def read_excel_data_to_numpy(excel_filename):
     """ Reads the given Excel file's data and returns the data in the form of a numpy array """
 
     # change directory to the Excel file directory
     os.chdir(EXCEL_DIR)
-    # return a panda dataframe with data from the given Excel file
-    excel_dataframe = pd.read_excel(excel_filename, sheet_name=sheet_name)
+
+    sheet_to_numpy_map = {}
+
+    # for every sheet in the Excel file
+    for sheet_name in pd.ExcelFile(excel_filename).sheet_names:
+        sheet_to_numpy_map[sheet_name] = pd.read_excel(excel_filename, sheet_name=sheet_name).to_numpy()
+
     os.chdir("..")
-    return excel_dataframe.to_numpy()
+    print("\n------------------------------------------------------")
+    print(f"Excel data for file \"{excel_filename}\":")
+    for sheet in sheet_to_numpy_map:
+        print(f"\nSheet: {sheet}")
+        print("Sheet Data:")
+        print(sheet_to_numpy_map[sheet])
+    print("------------------------------------------------------\n")
+    return sheet_to_numpy_map
 
 
-def convert_datetime(datetime):
-    """ converts given datetime.datetime parameter into the correct time format: [hh:mm:A/P] """
+def custom_time_format(excel_data, sheet, col, row, format_string):
+    """ formats the datetime data according to the given time format """
 
-    if isinstance(datetime, str):
-        return ""
-
-    hour = datetime.hour
-    if datetime.hour < 12:
-        is_time_am = True
-    elif datetime.hour == 12:
-        is_time_am = False
+    excel_time = excel_data[sheet][col][row]
+    if format_string == "default":
+        print("Defaulting to string conversion")
+        print("string conversion: %s" % str(excel_time))
+        return str(excel_time)
+    # if the Excel data at [col, row] is not a datetime object
     else:
-        hour -= 12
-        is_time_am = False
-    minute = datetime.minute
-    return f"{hour}:{minute}A" if is_time_am else f"{hour}:{minute}P"
+        result = excel_time.strftime(format_string)
+        print("formatted datetime: %s" % result)
+        return result
 
 
-def convert_numpy_to_excel_class(filename, excel_array):
-    """ converts the given numpy array containing data from an Excel file into an Excel class """
-
-    print(f"\nExtracting data from Excel file \"{filename}\":")
-    student_name = excel_array[4, 2]
-    reformatted_name_list = student_name.split(" ")
-    reformatted_name = reformatted_name_list[1] + ', ' + reformatted_name_list[0]
-    faculty_name = excel_array[5, 2]
-    course = excel_array[6, 2]
-    badge_num = int(excel_array[4, 9])
-    timesheet = {
-        "Fri": {"date": excel_array[9, 2],
-                "time_in_1": convert_datetime(excel_array[9, 3]),
-                "time_out_1": convert_datetime(excel_array[9, 4]),
-                "time_in_2": convert_datetime(excel_array[9, 5]),
-                "time_out_2": convert_datetime(excel_array[9, 6]),
-                "comments": excel_array[9, 8]},
-        "Sat": {"date": excel_array[10, 2],
-                "time_in_1": convert_datetime(excel_array[10, 3]),
-                "time_out_1": convert_datetime(excel_array[10, 4]),
-                "time_in_2": convert_datetime(excel_array[10, 5]),
-                "time_out_2": convert_datetime(excel_array[10, 6]),
-                "comments": excel_array[10, 8]},
-        "Sun": {"date": excel_array[11, 2],
-                "time_in_1": convert_datetime(excel_array[11, 3]),
-                "time_out_1": convert_datetime(excel_array[11, 4]),
-                "time_in_2": convert_datetime(excel_array[11, 5]),
-                "time_out_2": convert_datetime(excel_array[11, 6]),
-                "comments": excel_array[11, 8]},
-        "Mon": {"date": excel_array[12, 2],
-                "time_in_1": convert_datetime(excel_array[12, 3]),
-                "time_out_1": convert_datetime(excel_array[12, 4]),
-                "time_in_2": convert_datetime(excel_array[12, 5]),
-                "time_out_2": convert_datetime(excel_array[12, 6]),
-                "comments": excel_array[12, 8]},
-        "Tues": {"date": excel_array[13, 2],
-                 "time_in_1": convert_datetime(excel_array[13, 3]),
-                 "time_out_1": convert_datetime(excel_array[13, 4]),
-                 "time_in_2": convert_datetime(excel_array[13, 5]),
-                 "time_out_2": convert_datetime(excel_array[13, 6]),
-                 "comments": excel_array[13, 7]},
-        "Wed": {"date": excel_array[14, 2],
-                "time_in_1": convert_datetime(excel_array[14, 3]),
-                "time_out_1": convert_datetime(excel_array[14, 4]),
-                "time_in_2": convert_datetime(excel_array[14, 5]),
-                "time_out_2": convert_datetime(excel_array[14, 6]),
-                "comments": excel_array[14, 8]},
-        "Thurs": {"date": excel_array[15, 2],
-                  "time_in_1": convert_datetime(excel_array[15, 3]),
-                  "time_out_1": convert_datetime(excel_array[15, 4]),
-                  "time_in_2": convert_datetime(excel_array[15, 5]),
-                  "time_out_2": convert_datetime(excel_array[15, 6]),
-                  "comments": excel_array[15, 8]}
-    }
-
-    excel_data = ExcelData(faculty_name, course, reformatted_name, badge_num, timesheet)
-
-    print(f"Student Name: {excel_data.student_name}")
-    print(f"Faculty Name: {excel_data.faculty_name}")
-    print(f"Course: {excel_data.course}")
-    print(f"Badge Number: {excel_data.badge_num}")
-    print(f"Timesheet: {excel_data.timesheet}\n")
-
-    return excel_data
-
-
-def execute_timesheet(excel_class):
-    """ Executes the timesheet waypoints for the given Excel class
-    The user only needs to give the first location, as the rest of the
-    timesheet can be reached by hitting the 'tab' key on the keyboard """
-
-    def tab():
-        """ taps the 'tab' key, then waits """
-        kb.tap(Key.tab)
-        wait()
-
-    kb.type(excel_class.timesheet["Fri"]["time_in_1"])
-    tab()
-    kb.type(excel_class.timesheet["Fri"]["time_out_1"])
-    tab()
-    kb.type(excel_class.timesheet["Fri"]["time_in_2"])
-    tab()
-    kb.type(excel_class.timesheet["Fri"]["time_out_2"])
-    tab()
-    tab()
-    kb.type(excel_class.timesheet["Sat"]["time_in_1"])
-    tab()
-    kb.type(excel_class.timesheet["Sat"]["time_out_1"])
-    tab()
-    kb.type(excel_class.timesheet["Sat"]["time_in_2"])
-    tab()
-    kb.type(excel_class.timesheet["Sat"]["time_out_2"])
-    tab()
-    tab()
-    kb.type(excel_class.timesheet["Sun"]["time_in_1"])
-    tab()
-    kb.type(excel_class.timesheet["Sun"]["time_out_1"])
-    tab()
-    kb.type(excel_class.timesheet["Sun"]["time_in_2"])
-    tab()
-    kb.type(excel_class.timesheet["Sun"]["time_out_2"])
-    tab()
-    tab()
-    kb.type(excel_class.timesheet["Mon"]["time_in_1"])
-    tab()
-    kb.type(excel_class.timesheet["Mon"]["time_out_1"])
-    tab()
-    kb.type(excel_class.timesheet["Mon"]["time_in_2"])
-    tab()
-    kb.type(excel_class.timesheet["Mon"]["time_out_2"])
-    tab()
-    tab()
-    kb.type(excel_class.timesheet["Tues"]["time_in_1"])
-    tab()
-    kb.type(excel_class.timesheet["Tues"]["time_out_1"])
-    tab()
-    kb.type(excel_class.timesheet["Tues"]["time_in_2"])
-    tab()
-    kb.type(excel_class.timesheet["Tues"]["time_out_2"])
-    tab()
-    tab()
-    kb.type(excel_class.timesheet["Wed"]["time_in_1"])
-    tab()
-    kb.type(excel_class.timesheet["Wed"]["time_out_1"])
-    tab()
-    kb.type(excel_class.timesheet["Wed"]["time_in_2"])
-    tab()
-    kb.type(excel_class.timesheet["Wed"]["time_out_2"])
-    tab()
-    tab()
-    kb.type(excel_class.timesheet["Thurs"]["time_in_1"])
-    tab()
-    kb.type(excel_class.timesheet["Thurs"]["time_out_1"])
-    tab()
-    kb.type(excel_class.timesheet["Thurs"]["time_in_2"])
-    tab()
-    kb.type(excel_class.timesheet["Thurs"]["time_out_2"])
-    wait()
-    kb.tap(Key.enter)
-
-
-def execute_config(config_json, excel_class):
+def execute_config(config_json, excel_data):
     """ Executes the waypoints given by the user for a specific Excel class """
 
-    print(f"Executing waypoints for student \"{excel_class.student_name}\" with badge number {excel_class.badge_num}:")
+    print(f"Executing waypoints")
     for i in config_json:
-        # ignore the WAIT_DELAY_IN_SECONDS entry
-        if i == "WAIT_DELAY_IN_SECONDS":
+        # ignore the WAIT_DELAY_IN_SECONDS and DATETIME_FORMAT_STR entries
+        if i == "WAIT_DELAY_IN_SECONDS" or i == "DATETIME_FORMAT_STR":
             continue
         # click
-        if config_json[i]["type"] == "click":
+        elif config_json[i]["type"] == "click":
             print("Clicking at the point: [%f, %f]" % (config_json[i]["pos"][0], config_json[i]["pos"][1]))
             Mouse.position = config_json[i]["pos"]
             wait()
             Mouse.click(Button.left, 1)
         # double click
-        if config_json[i]["type"] == "double-click":
+        elif config_json[i]["type"] == "double-click":
             print("Double clicking at the point: [%f, %f]" % (config_json[i]["pos"][0], config_json[i]["pos"][1]))
             Mouse.position = config_json[i]["pos"]
             wait()
             Mouse.click(Button.left, 2)
-        # type student name
-        if config_json[i]["type"] == "student-name":
-            print(f"Typing student name (%s) at the point: [%f, %f]" % (excel_class.student_name, config_json[i]["pos"][0], config_json[i]["pos"][1]))
-            Mouse.position = config_json[i]["pos"]
-            wait()
-            Mouse.click(Button.left, 1)
-            wait()
-            kb.type(excel_class.student_name)
+        # paste a string
+        elif config_json[i]["type"] == "paste":
+            print("pasting \"%s\"")
+            kb.type(config_json[i]["paste"])
+        # tab
+        elif config_json[i]["type"] == "tab":
+            print("pressing the tab key")
+            kb.tap(Key.tab)
+        # enter
+        elif config_json[i]["type"] == "enter":
+            print("pressing the enter key")
             kb.tap(Key.enter)
-        # enter timesheet
-        if config_json[i]["type"] == "timesheet":
-            print("Entering student timesheet starting at the point: [%f, %f]" % (config_json[i]["pos"][0], config_json[i]["pos"][1]))
-            Mouse.position = config_json[i]["pos"]
-            wait()
-            Mouse.click(Button.left, 1)
-            wait()
-            execute_timesheet(excel_class)
+        # insert excel data
+        elif config_json[i]["type"] == "insert-data":
+            sheet = config_json[i]["sheet"]
+            col = config_json[i]["excel_col"] - 1
+            row = config_json[i]["excel_row"] - 1
+            print("inserting excel data at [sheet: %s, column %d, row %d]" % (sheet, col, row))
+            if isinstance(excel_data[sheet][col][row], datetime.datetime):
+                print("excel datetime data detected")
+                # type the formatted Excel data at the specified sheet, column and row
+                kb.type(custom_time_format(excel_data, sheet, col, row, DATETIME_FORMAT_STR))
+            else:
+                # type the Excel data at the specified sheet, column, and row
+                kb.type(str(excel_data[sheet][col][row]))
         # wait
-        if config_json[i]["type"] == "wait":
+        elif config_json[i]["type"] == "wait":
             print("waiting for %d seconds" % config_json[i]["seconds"])
             time.sleep(config_json[i]["seconds"])
+
         wait()
 
 
@@ -250,40 +118,25 @@ def execute(data):
     """ Extracts the data from all Excel files in the EXCEL_FILENAMES directory and
     executes the waypoints given by the user stored in the config.json file """
 
-    global WAIT_DELAY_IN_SECONDS
+    global WAIT_DELAY_IN_SECONDS, DATETIME_FORMAT_STR
 
     # Set the wait delay to the user's set wait delay
     WAIT_DELAY_IN_SECONDS = data["WAIT_DELAY_IN_SECONDS"]
+    # Set the datetime format string to the user's set datetime format string
+    DATETIME_FORMAT_STR = data["DATETIME_FORMAT_STR"]
 
     for file in EXCEL_FILENAMES:
-        execute_config(data, convert_numpy_to_excel_class(file, read_excel_data_to_numpy(file)))
+        execute_config(data, read_excel_data_to_numpy(file))
     print("\n Completed Excel data extraction and execution :)")
 
 
-def optional_reset_config():
-    """ Reset the wait time / waypoint configuration """
+def get_config_data():
+    """ Gets the configuration data from the config.json file """
 
     filename = "config.json"
-    if input("Reset program configuration? (y/n): ") == 'y':
-        # remove the previous config.json file
-        try:
-            os.remove(filename)
-            print("Replacing config.json file:")
-        except FileNotFoundError:
-            print("No config.json file to remove, continuing")
-
-        # RUN SETUP SCRIPT
-        # Store configuration in config.json
-        with open(filename, 'a+') as f:
-            json.dump(setup.config(), f, indent=4)
-
-        # get the new configuration from config.json
-        with open(filename, 'r') as f:
-            return json.load(f)
-    else:
-        # get the configuration from config.json
-        with open(filename, 'r') as f:
-            return json.load(f)
+    # get the configuration from config.json
+    with open(filename, 'r') as f:
+        return json.load(f)
 
 
 def execution_countdown(data):
@@ -292,7 +145,7 @@ def execution_countdown(data):
     if input("Run program using current configuration (y/n): ") == 'y':
         print("Countdown to executing waypoints:")
         for i in range(5):
-            print(5-i)
+            print(5 - i)
             time.sleep(1)
         print("\nExtracting data from Excel files and beginning waypoint execution:\n")
         # execute the configuration
@@ -300,9 +153,6 @@ def execution_countdown(data):
 
 
 if __name__ == "__main__":
-
-    # Allow the user to change wait time / waypoint configuration if desired
-    config_data = optional_reset_config()
     # Start countdown, then extract data from Excel files and execute the waypoints from the user's
     # desired configuration with the data from each Excel file.
-    execution_countdown(config_data)
+    execution_countdown(get_config_data())
